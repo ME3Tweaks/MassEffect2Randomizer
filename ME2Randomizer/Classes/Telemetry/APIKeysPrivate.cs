dc2c5ee042ed92e21c6b8b02ae0b108b621d022b
Mgamerz <mgamerz2@gmail.com>
2021-03-07 22:04:21 -0700

    Delete AC PK

--- ME2Randomizer/Classes/Telemetry/APIKeysPrivate.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ME2Randomizer.Classes.Telemetry
-{
-    public static partial class APIKeys
-    {
-        public static string Private_AppCenter { get; } = "510ff644-f67e-4a73-9115-b25733bac663";
-    }
-}
